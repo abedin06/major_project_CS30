634db541e5d996714a920ef1daef8ce1121dd901
Abedin7008752 <7008752@spsd.sk.ca>
2024-12-02 14:16:04 -0600

    worked on the planetary gravity

--- sketch.js
+++ sketch.js
@@ -8,8 +8,8 @@
 class SpaceShip{
   constructor(x,y){
     this.pos = createVector(x,y);
-    this.velx = createVector(0.0005,0);
-    this.vely = createVector(0,0.0005);
+    this.velx = createVector(5,0);
+    this.vely = createVector(0,5);
     this.length = 5;
     this.width = 2;
   }
@@ -24,20 +24,20 @@ class SpaceShip{
   }
 
   update_position(){
-    if (keyCode === 38){
-      this.pos.sub(this.vely);
+    if (keyCode === UP_ARROW){
+      this.pos.sub(0,5);
     }
 
-    if (keyCode === 40){
-      this.pos.add(this.vely);
+    if (keyCode === DOWN_ARROW){
+      this.pos.add(0,5);
     }
 
-    if(keyCode === 37){
-      this.pos.sub(this.velx);
+    if(keyCode === LEFT_ARROW){
+      this.pos.sub(5,0);
     }
 
-    if(keyCode === 39){
-      this.pos.add(this.velx);
+    if(keyCode === RIGHT_ARROW){
+      this.pos.add(5,0);
     }
   }
 
@@ -58,31 +58,50 @@ class Planet{
   applygravity(someShip, G){
     let gravity_acc = this.mass*G/ dist(this.x, this.y, someShip.pos.x, someShip.pos.y)**2;
     let angle = atan((someShip.pos.x-this.x)/(someShip.pos.y-this.y));
-    someShip.velx.add(gravity_acc*cos(angle), 0);
-    someShip.vely.add(0, gravity_acc*sin(angle));
+
+    if(someShip.pos.x < this.x && someShip.pos.y < this.y){
+      someShip.velx.add(gravity_acc*cos(angle), 0);
+      someShip.vely.sub(0, gravity_acc*sin(angle));
+    }
+
+    if(someShip.pos.x < this.x && someShip.pos.y > this.y){
+      someShip.velx.add(gravity_acc*cos(angle), 0);
+      someShip.vely.add(0, gravity_acc*sin(angle));
+    }
+
+    if(someShip.pos.x > this.x && someShip.pos.y > this.y){
+      someShip.velx.sub(gravity_acc*cos(angle), 0);
+      someShip.vely.add(0, gravity_acc*sin(angle));
+    }
+
+    if(someShip.pos.x > this.x && someShip.pos.y < this.y){
+      someShip.velx.sub(gravity_acc*cos(angle), 0);
+      someShip.vely.sub(0, gravity_acc*sin(angle));
+    }
+
   }
 
 }
 
 let player;
-const G_CONSTANT = 6.6743* 10**-2;
+const G_CONSTANT = 6.6743*10**-2;
 
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   angleMode(DEGREES);
-  player = new SpaceShip(0, height);
+  player = new SpaceShip(50, 100);
   mars = new Planet(width/2,height/2);
 }
 
 function draw() {
   background(220);
   //orbitControl();
-  player.display();
   mars.display();
-  player.move();
   mars.applygravity(player,G_CONSTANT);
   player.update_position();
+  player.move();
+  player.display();
 }
 
 
